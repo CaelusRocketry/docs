9d72c603dba319a6156926fdfcd10f67ab730b79
Mitali <mitali.gandhe@gmail.com>
2021-01-28 18:54:35 -0500

    Outline docs stucture, complete fs_setup

--- sidebars.js
+++ sidebars.js
@@ -8,6 +8,11 @@
 module.exports = {
     someSidebar: {
         Overview: ['index'],
-        "Ground Software (GS)": ['gs_setup', 'gs_backend', 'gs_frontend', 'gs_common_errors']
+        Propulsion: [],
+        Electronics: ['elec_launchbox'],
+        "Flight Software (FS)": ['fs_overview', 'fs_setup', 'fs_mcl', 'fs_tasks', 'fs_control_tasks', 'fs_drivers', 'fs_lib', 'fs_common_errors'],
+        "Ground Software (GS)": ['gs_setup', 'gs_backend', 'gs_frontend', 'gs_common_errors'],
+        Website: ['web_frontend', 'web_sanity']
+    
     }
 };
