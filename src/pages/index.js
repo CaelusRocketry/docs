7ce5c8ec9696aa09655b4d19f68daadf78594da1
Akash Bhave <akashbbhave@gmail.com>
2021-02-09 12:05:49 -0500

    Update workflow, dynamically set logo for dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,22 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import useThemeContext from "@theme/hooks/useThemeContext";
 import styles from "./index.module.css";
+
+const Logo = () => {
+  const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
+  return (
+    <img
+      src={isDarkTheme ? "/img/logo-dark.svg" : "/img/logo.svg"}
+      alt="Logo"
+    />
+  );
+};
 
 const IndexPage = () => {
   const context = useDocusaurusContext();
+
   const { siteConfig = {} } = context;
   return (
     <Layout
@@ -16,7 +28,7 @@ const IndexPage = () => {
     >
       <main className={styles}>
         <section className={styles.top}>
-          <img src="/img/logo.svg"></img>
+          <Logo />
           <p>Welcome to Project Caelus' documentation site!</p>
         </section>
       </main>
