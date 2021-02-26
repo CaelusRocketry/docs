580b4c671324d5bca28942bdbb79666a4d778c9c
Ron Nachum <ronnachum13@gmail.com>
2021-02-25 21:20:58 -0500

    Add info to nozzle.mdx, injector.mdx. Also add design_overview, parameter_overview, and more

---
+++ docs/propulsion/aphlex/design_overview.mdx
@@ -0,0 +1,4 @@
+---
+title: Aphlex 1C Engine - Design Overview
+sidebar_label: Design Overview
+---
--- sidebars.js
+++ sidebars.js
@@ -1,17 +1,15 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
   docs: {
     Overview: ["index"],
     Propulsion: [
       {
         "Aphlex Engine Design": [
-            "overview",
+            "parameter_overview",
+            "design_overview",
+            "inputs",
+            "cea",
+            "nozzle",
+            "injector",
         ].map((page) => `propulsion/aphlex/${page}`),
       },
       {
@@ -42,4 +40,5 @@ module.exports = {
     ],
     Website: ["front-end", "cms"].map((page) => `website/${page}`),
   },
-};
+};26
+// uh oh stinky
